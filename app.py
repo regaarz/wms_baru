fb35fba418ec006c71e1f24dec203f18da4f1d72
rega.arz <regaarzulaakbar93@gmail.com>
2025-08-07 19:22:03 +0700

    added row to DB

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect,url_for
+from db import getProduct, addTask
 
 app = Flask(__name__)
 
-product = [["oil",True],['palu',True],['kampak',False]]
 
 @app.route("/")
-def honme():
+def home():
+    product = getProduct()
     return render_template("wms.html", Product = product)
 
+@app.route("/add", methods=['POST'])
+def add():
+    productname = request.form['productName']
+    categoryproduct = request.form['categoryProduct']
+    duedate = request.form['dueDate']
+    status_ = request.form['status']
+    addTask(productname,categoryproduct,duedate,status_)
+    return redirect(url_for('home'))
 
 app.run()
--- db.py
+++ db.py
@@ -7,8 +7,16 @@ db_host = "localhost"
 def getProduct():
     conn = psycopg2.connect(dbname = db_name,user=db_user,password=db_pw,host = db_host)
     cur = conn.cursor()
-    cur.execute('SELECT product_id, category_product,due_date, status FROM public."product" ')
+    cur.execute('SELECT product_id, product_name, category_product,due_date, created_at, update_at, status FROM public."product_warehouse" ')
     product_list = cur.fetchall()
     cur.close
     conn.close
     return product_list
+
+def addTask(product,category,date,status):
+    conn = psycopg2.connect(dbname = db_name,user=db_user,password=db_pw,host = db_host)
+    cur = conn.cursor()
+    cur.execute('INSERT INTO public."product_warehouse"(product_name, category_product, created_at, update_at, due_date, status) VALUES (%s, %s, NOW(), NOW(), %s, %s)', (product,category,date,status))
+    conn.commit() 
+    cur.close
+    conn.close
